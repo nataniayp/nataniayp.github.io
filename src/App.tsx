466f1fb60fe22ab088232d98c7dbed594cb08cac
Natania <natania.yovela@gmail.com>
2023-11-24 00:48:05 +0800

    Replace BrowserRouter with HashRouter for gh-pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { Home } from './pages/Home';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-import { NavBar } from './components/NavBar';
-import { Portfolio } from './pages/Portfolio';
-import { Blog } from './pages/Blog';
-import { Contact } from './pages/Contact';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { NavBar } from "./components/NavBar";
+import { Portfolio } from "./pages/Portfolio";
+import { Blog } from "./pages/Blog";
+import { Contact } from "./pages/Contact";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#00873d',
-      dark: '#004921',
-      light: '#97bfa9',
+      main: "#00873d",
+      dark: "#004921",
+      light: "#97bfa9",
     },
     secondary: {
-      main: '#fb8d22',
-      dark: '#c46508',
-      light: '#fab673',
+      main: "#fb8d22",
+      dark: "#c46508",
+      light: "#fab673",
     },
   },
   typography: {
     // fontFamily: 'Helvetica Neue'
-  }
-})
-
+  },
+});
 
 function App() {
   return (
