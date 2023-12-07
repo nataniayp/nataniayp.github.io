e93939209b38194c746f2646516ad16ee6bff7a5
Natania <natania.yovela@gmail.com>
2023-12-07 19:58:36 +0800

    Update Contact page, integrated with db

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,14 +1,16 @@
 import React, { useState, ChangeEvent } from "react";
-import { Link } from "react-router-dom";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import styled, { css } from "styled-components";
 import SendIcon from "@mui/icons-material/Send";
+import { LoadingButton } from "@mui/lab";
+import axios from "axios";
 
-const STypography = styled(Typography)`
+const SLoadingButton = styled(LoadingButton)`
   ${(_) => css`
     margin-top: 12px;
     display: flex;
+    justify-content: start;
     align-items: center;
     text-decoration: none;
     font-weight: 600;
@@ -27,25 +29,87 @@ function isValidEmail(email: string): boolean {
 export const Contact = () => {
   const theme = useTheme();
   const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
   const [email, setEmail] = useState("");
-  const [nessage, setMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
   const [emailError, setEmailError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    setName(event.target.value);
+    const newName = event.target.value;
+    setName(newName);
+    if (newName) {
+      setNameError("");
+    }
   };
 
   const handleEmailChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.value && !isValidEmail(event.target.value)) {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    if (newEmail && !isValidEmail(newEmail)) {
       setEmailError("Invalid email address");
     } else {
-      setEmail(event.target.value);
       setEmailError("");
     }
   };
 
   const handleMessageChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setMessage(event.target.value);
+  };
+
+  const resetForm = () => {
+    setLoading(false);
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  const handleSubmit = (): void => {
+    if (!name) {
+      setNameError("Empty name");
+    }
+
+    if (!email || emailError) {
+      const error = emailError === "" ? "Empty email address" : emailError;
+      setEmailError(error);
+    }
+
+    if (name && email && !nameError && !emailError) {
+      try {
+        setLoading(true);
+        const contactMessage = {
+          name: name,
+          email: email,
+          message: message,
+        };
+
+        setTimeout(() => {
+          axios
+            .post(
+              "https://personal-site-db.onrender.com/contacts",
+              contactMessage
+            )
+            .then((response) => {
+              console.log(response.data);
+              console.log(response.status, response.data.token);
+              setSuccessMessage("Thanks, " + name + "!");
+              console.log("set success 1");
+              resetForm();
+
+              setTimeout(() => {
+                setSuccessMessage("");
+              }, 2000);
+            });
+        }, 2000);
+      } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+          console.log("error message: ", error.message);
+        } else {
+          console.log("unexpected error: ", error);
+        }
+      }
+    }
   };
 
   return (
@@ -72,32 +136,47 @@ export const Contact = () => {
           >
             Let's connect!
           </Box>
-          <TextField required label="Name" onChange={handleNameChange} />
+          <TextField
+            required
+            label="Name"
+            value={name}
+            error={!!nameError}
+            onChange={handleNameChange}
+            helperText={nameError}
+          />
 
           <TextField
             required
             error={!!emailError}
+            value={email}
             label="Email Address"
             onChange={handleEmailChange}
             helperText={emailError}
           />
 
-          <TextField multiline label="Message" onChange={handleMessageChange} />
-
-          <STypography
-            color={theme.palette.primary.main}
-            component={Link}
-            to={"mailto:natania.yovela@gmail.com"}
+          <TextField
+            multiline
+            label="Message"
+            value={message}
+            onChange={handleMessageChange}
+          />
+          <SLoadingButton
+            loading={loading}
+            disableRipple
+            onClick={handleSubmit}
+            startIcon={
+              <SendIcon
+                sx={{
+                  color: theme.palette.primary.main,
+                  height: "12px",
+                }}
+              />
+            }
+            loadingPosition="start"
           >
-            <SendIcon
-              sx={{
-                color: theme.palette.primary.main,
-                height: "12px",
-                marginRight: "2px",
-              }}
-            />
-            Send
-          </STypography>
+            <span>Send</span>
+          </SLoadingButton>
+          {successMessage && <Typography>{successMessage}</Typography>}
         </Box>
       </Box>
     </Container>
