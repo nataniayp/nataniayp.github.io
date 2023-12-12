b057e0074d3c0ec65af3ee439444f517d663cccf
Natania <natania.yovela@gmail.com>
2023-12-12 20:44:33 +0800

    Add mobile ver of Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Box, Container, Link, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import profilePicture from "../assets/homepage.png";
-import keyboardPicture from "../assets/keyboard.png";
 import styled, { css } from "styled-components";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const SImg = styled.img`
   object-fit: cover;
@@ -18,7 +18,37 @@ const SLink = styled(Link)<{ textColor: string }>`
 
 export const Home = () => {
   const theme = useTheme();
-  return (
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  return matches ? (
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="start"
+        alignItems="center"
+      >
+        <SImg height="300px" width="300px" src={profilePicture} />
+
+        <Box
+          display="flex"
+          marginBottom="4px"
+          sx={{
+            fontWeight: 700,
+            fontSize: "2rem",
+            color: theme.palette.primary.main,
+          }}
+        >
+          Hi, I am Natania.
+        </Box>
+        <Typography align="center">
+          I like doing web development, like this website!
+        </Typography>
+        <Typography align="center">
+          Click any of the tabs on top to find out more about me.
+        </Typography>
+      </Box>
+    </Container>
+  ) : (
     <Container>
       <Box
         marginX="4rem"
@@ -53,30 +83,6 @@ export const Home = () => {
           <SImg height="400em" width="400em" src={profilePicture} />
         </Box>
       </Box>
-      {/* <Box display="flex" alignItems="center">
-        <SImg height="300em" src={keyboardPicture} />
-        <Box display="flex" flexDirection="column" marginLeft="32px">
-          <Typography>I play the keyboard.</Typography>
-          <Typography>
-            Currently, I am in{" "}
-            <SLink
-              textColor="#E24900"
-              underline="hover"
-              href="https://instagram.com/officiallyaight"
-            >
-              aight.
-            </SLink>{" "}
-            and{" "}
-            <SLink
-              textColor="#E24900"
-              underline="hover"
-              href="https://instagram.com/wearematb"
-            >
-              Marisa and the Baristas.
-            </SLink>
-          </Typography>
-        </Box>
-      </Box> */}
     </Container>
   );
 };
