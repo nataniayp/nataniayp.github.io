dde3f58aec6075fe79bc87802dee3d37f0bc4a00
Natania <natania.yovela@gmail.com>
2023-12-15 16:19:12 +0800

    Add drawer for mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-import { AppBar, Stack, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const STypography = styled(Typography)`
   ${(_) => css`
@@ -22,7 +33,71 @@ const pages = [
 ];
 
 export const NavBar = () => {
-  return (
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setIsDrawerOpen(open);
+    };
+
+  return isMobile ? (
+    <AppBar position="sticky" sx={{ bgcolor: "white", boxShadow: "none" }}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        marginY="8px"
+        marginX="16px"
+        alignItems="center"
+      >
+        <Box
+          sx={{
+            fontWeight: 700,
+            fontSize: "20px",
+            letterSpacing: "4px",
+            color: theme.palette.primary.main,
+          }}
+        >
+          NATANIAYP
+        </Box>
+        <IconButton onClick={toggleDrawer(true)} edge="end">
+          <MenuIcon sx={{ color: theme.palette.primary.main }} />
+        </IconButton>
+      </Box>
+      <Drawer anchor="right" open={isDrawerOpen} onClose={toggleDrawer(false)}>
+        <Stack
+          marginTop={4}
+          marginBottom={3}
+          spacing={8}
+          direction={"column"}
+          marginRight="64px"
+          color="black"
+          marginLeft="32px"
+        >
+          {pages.map((page) => (
+            <STypography
+              key={page.to}
+              component={Link}
+              to={page.to}
+              onClick={toggleDrawer(false)}
+            >
+              {page.label}
+            </STypography>
+          ))}
+        </Stack>
+      </Drawer>
+    </AppBar>
+  ) : (
     <AppBar position="sticky" sx={{ bgcolor: "white", boxShadow: "none" }}>
       <Toolbar>
         <Stack
